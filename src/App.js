6ab70f52416cdc78f3a5707bf3a9cc377fe57d5e
hasanTheBest <rainbowmahmud@gmail.com>
2022-04-10 01:59:00 +0600

    sign in ui added.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ import {
 } from "firebase/auth";
 import { useState } from "react";
 
-function App() {
-  const auth = getAuth(app);
+const auth = getAuth(app);
 
+function App() {
   const [user, setUser] = useState({});
+  const [emailPass, setEmailPass] = useState({});
+
+  const handleInputBlur = (e) =>
+    setEmailPass({ ...emailPass, [e.target.name]: e.target.value });
+
+  const handleClickSignIn = (e) => {
+    e.preventDefault();
+    console.log("handle click singin");
+  };
 
   const handleClick = (name) => {
     if (name === "google") {
@@ -22,7 +31,7 @@ function App() {
         .then((res) => setUser(res.user))
         .catch((err) => console.error(err.message));
     } else {
-      console.log("facebook provider");
+      console.log("email and password");
     }
   };
 
@@ -33,16 +42,110 @@ function App() {
           Firebase Authentication
         </h1>
 
-        {/* auth trigger button */}
-        <div className="shadow flex gap-3 items-center">
-          <h3 className="p-6 text-center text-emerald-700 text-3xl font-semibold bg-emerald-100">
-            Sign in with
-          </h3>
-          {/* button container */}
-          <div className="flex gap-3 flex-grow-1">
-            <button
-              className="p-3 flex gap-2 bg-emerald-700 hover:bg-emerald-600 text-emerald-200 hover:text-emerald-50 transition-all rounded"
-              onClick={() => handleClick("google")}
+        {/* container for sign in form */}
+        <div className="flex gap-6">
+          {/* Sing in with */}
+          <form
+            className="bg-emerald-200 p-4 rounded flex-grow-1 w-1/2 items-center"
+            onSubmit={handleClickSignIn}
+          >
+            {/* Email */}
+            <div className="mb-4 flex flex-col">
+              <label
+                htmlFor="email"
+                className="text-emerald-800 font-semibold mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="p-3 text-emerald-900 font-semibold bg-emerald-100 border-0 rounded shadow w-full"
+                placeholder="Your Email"
+                name="email"
+                onBlur={handleInputBlur}
+                required
+              />
+            </div>
+
+            {/* Password */}
+            <div className="mb-4 flex flex-col">
+              <label
+                htmlFor="password"
+                className="text-emerald-800 font-semibold mb-2"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="p-3 text-emerald-900 font-semibold bg-emerald-100 border-0 rounded shadow w-full"
+                name="password"
+                onBlur={handleInputBlur}
+                required
+              />
+            </div>
+
+            {/* submit button */}
+            <input
+              className="p-3 bg-emerald-700 hover:bg-emerald-600 hover:cursor-pointer font-semibold text-emerald-200 hover:text-emerald-50 transition-all rounded shadow w-full"
+              type="submit"
+              value="Sing In"
+            />
+          </form>
+
+          {/* auth trigger button */}
+          <div className="shadow flex flex-col gap-3 items-center bg-emerald-100 p-4 w-1/2 justify-center">
+            <h3 className="p-6 text-center text-emerald-700 text-3xl font-semibold bg-emerald-100">
+              Sign in with
+            </h3>
+            {/* button container */}
+            <div className="flex gap-3 flex-grow-1">
+              {/* Google Button */}
+              <button
+                className="p-3 flex gap-2 bg-emerald-700 hover:bg-emerald-600 text-emerald-200 hover:text-emerald-50 transition-all rounded"
+                onClick={() => handleClick("google")}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                  />
+                </svg>
+                Google
+              </button>
+              {/* Github */}
+              <button
+                className="p-3 flex gap-2 bg-slate-700 hover:bg-slate-600 text-slate-200 hover:text-slate-50 transition-all rounded"
+                onClick={() => handleClick("github")}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                  />
+                </svg>
+                Github
+              </button>
+
+              {/* <button
+              className="p-3 flex gap-2 bg-fuchsia-700 hover:bg-fuchsia-600 text-fuchsia-200 hover:text-sky-50 transition-all rounded"
+              onClick={() => handleClick("emailAndPassword")}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -58,48 +161,9 @@ function App() {
                   d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
                 />
               </svg>
-              Google
-            </button>
-            <button
-              className="p-3 flex gap-2 bg-sky-700 hover:bg-sky-600 text-sky-200 hover:text-sky-50 transition-all rounded"
-              onClick={() => handleClick("facebook")}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                />
-              </svg>
-              Facebook
-            </button>
-            <button
-              className="p-3 flex gap-2 bg-slate-700 hover:bg-slate-600 text-slate-200 hover:text-slate-50 transition-all rounded"
-              onClick={() => handleClick("github")}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                />
-              </svg>
-              Github
-            </button>
+              Email and Password
+            </button> */}
+            </div>
           </div>
         </div>
 
